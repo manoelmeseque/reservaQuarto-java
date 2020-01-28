e39e2fcd6d41ce688444fe09e143b5f00a893a83
manoelmeseque <meseque.teixeira@outlook.com>
2020-01-28 15:29:45 -0300

    Solução muito ruim

---
+++ src/application/Main.java
@@ -0,0 +1,48 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import model.entities.Reserva;
+
+public class Main {
+
+	public static void main(String[] args) throws ParseException {
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		System.out.print("Numero do quarto: ");
+		int numero = sc.nextInt();
+		System.out.print("Cleck-In (dd/MM/yyyy): ");
+		Date checkIn = sdf.parse(sc.next());
+		System.out.print("Cleck-Out (dd/MM/yyyy): ");
+		Date checkOut = sdf.parse(sc.next());
+		
+		if(!checkOut.after(checkIn)) {
+			System.out.println("Erro: O check-Out não pode ser antes do check-In");
+		}else {
+			Reserva reserva = new Reserva(numero, checkIn, checkOut);
+			System.out.println("Reserva: " + reserva);
+			System.out.println();
+			System.out.print("Cleck-In (dd/MM/yyyy): ");
+			checkIn = sdf.parse(sc.next());
+			System.out.print("Cleck-Out (dd/MM/yyyy): ");
+			checkOut = sdf.parse(sc.next());
+			
+			Date agora = new Date();
+			if(checkIn.before(agora) || checkOut.before(agora)) {
+				System.out.println("Erro: So pode usar uma data futura");
+			}else if(!checkOut.after(checkIn)) {
+				System.out.println("Erro: O check-Out não pode ser antes do check-In");
+			}else {
+				reserva.atualizaData(checkIn, checkOut);
+				System.out.print(reserva);
+			}
+			
+		}
+		
+		sc.close();
+	}
+
+}
---
+++ src/model/entities/Reserva.java
@@ -0,0 +1,65 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reserva {
+	private Integer numeroQuarto;
+	private Date checkIn;
+	private Date checkOut;
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Reserva() {
+		
+	}
+	
+	public Reserva(Integer numeroQuarto, Date checkIn, Date checkOut) {
+		this.numeroQuarto = numeroQuarto;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	public Integer getNumeroQuarto() {
+		return numeroQuarto;
+	}
+
+	public void setNumeroQuarto(Integer numeroQuarto) {
+		this.numeroQuarto = numeroQuarto;
+	}
+
+	public Date getCheckIn() {
+		return checkIn;
+	}
+
+	public Date getCheckOut() {
+		return checkOut;
+	}
+	
+	public long duracao() {
+		long d = checkOut.getTime() - checkIn.getTime();
+		return TimeUnit.DAYS.convert(d, TimeUnit.MILLISECONDS);
+	}
+	
+	public void atualizaData(Date checkIn, Date checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	@Override
+	public String toString() {
+		return "Número do quarto: "
+				+ numeroQuarto
+				+ ", Check-In: "
+				+ sdf.format(checkIn)
+				+ ", Check-Out: "
+				+ sdf.format(checkOut)
+				+ ", "
+				+ duracao()
+				+ " Dias";
+	}
+
+
+	
+}
